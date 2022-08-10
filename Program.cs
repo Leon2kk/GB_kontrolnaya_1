6d318bee682cd71fdfc8001b375605338903db4c
DOOM\user <igruppa4@mail.ru>
2022-08-10 09:22:54 +0300

    Added source file of algorithm (C# format)

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GB
+{
+    // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+
+    class kr_1
+    {
+        public static void Main()
+        {
+            Console.Clear();
+           
+            string[]  mas = new string[] {"hello", "2", "world", ":-)"},
+                      res = new string[mas.Length];
+            
+            int i = 0;
+            foreach(string item in mas)
+            {
+                if (item.Length <= 3)
+                {
+                    res[i++] = item;
+                }
+            }
+
+            res = res.Where(item => item != null).ToArray();
+
+            Console.WriteLine("[" +String.Join(", ", mas) +"] -> [" + String.Join(", ", res)+"] \n");
+        }
+    }
+}
